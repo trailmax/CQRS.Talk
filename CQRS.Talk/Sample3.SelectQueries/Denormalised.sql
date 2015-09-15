3bc26c7967e8a4af50eb97331eff59d7ba9d2aa9
trailmax <trailmax1@gmail.com>
2015-09-15 23:51:44 +0100

    Added sample 3

---
+++ CQRS.Talk/Sample3.SelectQueries/Denormalised.sql
@@ -0,0 +1,3 @@
+﻿
+
+select * from TrainingGapsReport
---
+++ CQRS.Talk/Sample3.SelectQueries/ReadFromModel.sql
@@ -0,0 +1,13 @@
+﻿
+
+select 	*
+from people p
+    inner join JOBDETAIL jd on p.people_id = jd.people_id
+    inner join POST on jd.JOBTITLE = post.POST_ID
+    inner join ORGLEVEL3 o3 on jd.orglevel3 = o3.orglevel3_id
+    inner join ORGLEVEL2 o2 on o3.Orglevel2 = o2.orglevel2_id
+    inner join region r on o2.region = r.region_id
+where p.[status]='Active'
+    and jd.principaljob='T'
+    and jd.CURRENTRECORD='YES'
+    and post.[Type] = 'Operational'
