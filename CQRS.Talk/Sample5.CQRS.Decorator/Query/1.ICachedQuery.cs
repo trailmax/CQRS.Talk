f185bd1c30ad35415d4ece4b995d7861e3a6514f
trailmax <trailmax1@gmail.com>
2015-09-18 16:26:57 +0100

    Cached query decorator

---
+++ CQRS.Talk/Sample5.CQRS.Decorator/Query/1.ICachedQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Talk.Sample5.CQRS.Decorator.Query
+{
+    public interface ICachedQuery
+    {
+        String CacheKey { get; }
+        TimeSpan CacheDuration { get; }
+    }
+
+}
---
+++ CQRS.Talk/Sample5.CQRS.Decorator/Query/2.CachedQueryHandlerDecorator.cs
@@ -0,0 +1,55 @@
+﻿// ReSharper disable SuspiciousTypeConversion.Global
+using System;
+using CQRS.Talk.Refactoring1.Queries.Step5.Mediator;
+
+
+namespace CQRS.Talk.Sample5.CQRS.Decorator.Query
+{
+    public class CachedQueryHandlerDecorator<TQuery, TResult> : IQueryHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        private readonly IQueryHandler<TQuery, TResult> decorated;
+        private readonly ICacheProvider cacheProvider;
+
+        public CachedQueryHandlerDecorator(IQueryHandler<TQuery, TResult> decorated, ICacheProvider cacheProvider)
+        {
+            this.decorated = decorated;
+            this.cacheProvider = cacheProvider;
+        }
+
+
+        public TResult Request(IQuery<TResult> query)
+        {
+            var cachedQuery = query as ICachedQuery;
+
+            if (cachedQuery == null)
+            {
+                // query is not cached - just executed the actual query handler
+                return decorated.Request(query);
+            }
+
+            var cacheKey = cachedQuery.CacheKey;
+            var cachedObject = cacheProvider.Get(cacheKey); // try get query results from the cache
+
+            if (cachedObject != null && cachedObject is TResult)
+            {
+                // got object from the cache
+                return (TResult)cachedObject;
+            }
+            // cache contains nothing for this key
+
+            // requrest actual query handler for the query results
+            var cachedResult = decorated.Request(query);
+
+            // save the result into cache
+            cacheProvider.Set(cachedQuery.CacheKey, cachedResult, cachedQuery.CacheDuration);
+            return cachedResult;
+        }
+    }
+
+
+    public  interface ICacheProvider
+    {
+        object Get(string cacheKey);
+        void Set(string cacheKey, object cachedResult, TimeSpan cacheDuration);
+    }
+}
