e38d30e924afd7fb87ec76fed42b0a8444c2c9a4
trailmax <trailmax1@gmail.com>
2015-09-18 01:13:45 +0100

    Refactoring2 - string

---
+++ CQRS.Talk/Refactoring2.Queries/0.Dependencies.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Talk.Refactoring2.Queries
+{
+    public class CreateWorkMovementCommand
+    {
+    }
+
+
+    public class DomainException : Exception
+    {
+        public DomainException(string movementTypeTrainingIsNotFoundPleaseCreateIt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public class SessionDelegate
+    {
+        public SessionDelegate(ISessionDelegateCreate delegateData)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public int SessionDelegateId { get; set; }
+        public int PersonId { get; set; }
+
+
+        public void Update(ISessionDelegateUpdate delegateData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+    public interface IWorkMovementService
+    {
+        void UpdateWorkMovementComment(int sessionDelegateId, string trainingSessionIsMarkedAsNoShow);
+        void DeleteTrainingMovement(object sessionDelegateId);
+        void CreateWorkMovement(CreateWorkMovementCommand createWorkMovementCommand);
+    }
+
+    public class TrainingCourseAccommodationBooking
+    {
+        public int SessionDelegateId { get; set; }
+        public int TrainingCourseAccommodationBookingId { get; set; }
+        public string Notes { get; set; }
+    }
+
+    public interface ITrainingCourseAccommodationBookingRepository
+    {
+        void Save();
+        void Update(object accommodationBooking);
+        IEnumerable<TrainingCourseAccommodationBooking> All { get; }
+        void Delete(int trainingCourseAccommodationBookingId);
+    }
+
+
+    public interface IPersonRepository
+    {
+        SessionDelegate Find(int personId);
+    }
+
+
+    public class CourseSession
+    {
+        public int CourseSessionId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+    public interface ICourseSessionRepository
+    {
+        IEnumerable<CourseSession> All { get; set; }
+    }
+
+
+    public interface IMovementTypeRepository
+    {
+        IEnumerable<MovementType> All { get; }
+    }
+    public class MovementType {
+        public String Name { get; set; }
+    }
+
+
+
+    public interface ISessionDelegateRepository
+    {
+        void Insert(SessionDelegate sessionDelegate);
+        void Save();
+        SessionDelegate Find(int sessionDelegateId);
+        void Update(SessionDelegate sessionDelegate);
+    }
+
+    public interface ISessionDelegateUpdate : ISessionDelegateCreate
+    {
+        int SessionDelegateId { get; set; }
+    }
+
+
+    public interface ISessionDelegateCreate
+    {
+        int CourseSessionId { get; set; }
+        int PersonId { get; set; }
+    }
+}
---
+++ CQRS.Talk/Refactoring2.Queries/1.Service.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CQRS.Talk.Refactoring2.Queries
+{
+    public interface ITrainingService
+    {
+        int AddDelegateToSession(ISessionDelegateCreate delegateData);
+        void UpdateDelegateFromSession(ISessionDelegateUpdate delegateData);
+        void CancelDelegateFromSession(int delegateId);
+        void SessionDelegateNoShow(int sessionDelegateId);
+    }
+
+
+    public class TrainingService : ITrainingService
+    {
+        private readonly ISessionDelegateRepository sessionDelegateRepository;
+        private readonly IMovementTypeRepository movementTypeRepository;
+        private readonly ICourseSessionRepository sessionRepository;
+        private readonly IPersonRepository personRepository;
+        private readonly ITrainingCourseAccommodationBookingRepository accommodationBookingRepository;
+        private readonly IWorkMovementService workMovementService;
+
+
+        public TrainingService(
+            ISessionDelegateRepository sessionDelegateRepository,
+            IMovementTypeRepository movementTypeRepository,
+            ICourseSessionRepository sessionRepository,
+            IPersonRepository personRepository,
+            IWorkMovementService workMovementService,
+            ITrainingCourseAccommodationBookingRepository accommodationBookingRepository)
+        {
+            this.sessionDelegateRepository = sessionDelegateRepository;
+            this.movementTypeRepository = movementTypeRepository;
+            this.sessionRepository = sessionRepository;
+            this.personRepository = personRepository;
+            this.workMovementService = workMovementService;
+            this.accommodationBookingRepository = accommodationBookingRepository;
+        }
+
+
+        public int AddDelegateToSession(ISessionDelegateCreate delegateData)
+        {
+            var sessionDelegate = new SessionDelegate(delegateData);
+            sessionDelegateRepository.Insert(sessionDelegate);
+            sessionDelegateRepository.Save();
+
+            var createWorkMovementCommand = this.CreateWorkMovementCommand(delegateData, sessionDelegate);
+
+            workMovementService.CreateWorkMovement(createWorkMovementCommand);
+
+            return sessionDelegate.SessionDelegateId;
+        }
+
+
+        private CreateWorkMovementCommand CreateWorkMovementCommand(ISessionDelegateCreate delegateData, SessionDelegate sessionDelegate)
+        {
+            var session = sessionRepository.All.FirstOrDefault(s => s.CourseSessionId == delegateData.CourseSessionId);
+            var person = personRepository.Find(delegateData.PersonId);
+            var movementType = this.movementTypeRepository.All.FirstOrDefault(t => t.Name == "Training");
+
+            var createWorkMovementCommand = new CreateWorkMovementCommand()
+            {
+                // do some mapping
+            };
+            return createWorkMovementCommand;
+        }
+
+
+        public void UpdateDelegateFromSession(ISessionDelegateUpdate delegateData)
+        {
+            var sessionDelegate = sessionDelegateRepository.Find(delegateData.SessionDelegateId);
+            sessionDelegate.Update(delegateData);
+            sessionDelegateRepository.Update(sessionDelegate);
+            sessionDelegateRepository.Save();
+
+            var createWorkMovementCommand = this.CreateWorkMovementCommand(delegateData, sessionDelegate);
+            workMovementService.CreateWorkMovement(createWorkMovementCommand);
+        }
+
+
+        public void CancelDelegateFromSession(int delegateId)
+        {
+            var sessionDelegate = sessionDelegateRepository.Find(delegateId);
+
+            if (sessionDelegate == null)
+            {
+                return;
+            }
+
+            workMovementService.DeleteTrainingMovement(sessionDelegate.SessionDelegateId);
+
+            // remove booked accommodation
+            var accommodationBooking = accommodationBookingRepository.All.FirstOrDefault(b => b.SessionDelegateId == sessionDelegate.SessionDelegateId);
+            if (accommodationBooking != null)
+            {
+                accommodationBookingRepository.Delete(accommodationBooking.TrainingCourseAccommodationBookingId);
+            }
+        }
+
+
+        public void SessionDelegateNoShow(int sessionDelegateId)
+        {
+            workMovementService.UpdateWorkMovementComment(sessionDelegateId, "Training session is marked as No Show");
+
+            // update accommodation booking note
+            var accommodationBooking = accommodationBookingRepository.All.FirstOrDefault(b => b.SessionDelegateId == sessionDelegateId);
+            if (accommodationBooking != null)
+            {
+                accommodationBooking.Notes = String.Format("{0}{1}{1}{2}", "Session Delegate is marked as No Show",
+                                                           Environment.NewLine, accommodationBooking.Notes);
+                accommodationBookingRepository.Update(accommodationBooking);
+                accommodationBookingRepository.Save();
+            }
+        }
+    }
+}
